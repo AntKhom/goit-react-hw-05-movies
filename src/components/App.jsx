cec037c6f94e9a8f8c42cf3dc8c008a6eed3a376
AntKhom <antonhomenko1988@gmail.com>
2023-11-08 10:33:00 -0500

    layout

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,18 @@
+import { NavLink, Route, Routes } from "react-router-dom";
+import { SharedLayout } from "./SharedLayout";
+
+
 export const App = () => {
   return (
     <div>
-      React homework template
+      <Routes>
+        <Route path="/" element={<SharedLayout/>}>
+          <Route path="/" element={<div>Home</div>}></Route>
+          <Route path="/movies" element={<div>Movies</div>}></Route>
+          <Route path="/movies/:movieId" element={<div>MovieId</div>}></Route>
+        </Route>
+      </Routes>
+
     </div>
   );
 };
